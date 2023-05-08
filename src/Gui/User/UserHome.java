fceda9e8c537e3a185d2c5611f381d1b5b8ecc14
abirmachraoui <abir.machraoui@hotmail.com>
2023-05-08 23:21:12 +0100

    add missing

--- src/Gui/User/UserHome.java
+++ src/Gui/User/UserHome.java
@@ -5,10 +5,29 @@
  */
 package Gui.User;
 
+import com.codename1.ui.Command;
+import com.codename1.ui.Form;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.layouts.BoxLayout;
+
 /**
  *
  * @author hanin
  */
-public class UserHome {
+public class UserHome extends Form{
+    public UserHome(int id){
+        setTitle("Home Page");
+        setLayout(BoxLayout.yCenter());
+        Form edit=new EditProfile(1);
+        add(edit);
+        //Form F1=new ChangePassword();
+        Command modifPass = new Command("Modifier mot de passe") {
+        @Override
+        public void actionPerformed(ActionEvent evt) {
+         new EditPassword(1).show();
+    }
+};
+        getToolbar().addCommandToSideMenu(modifPass);
+    }
     
 }
